06b31f2be7898fac943abfdeb0dc76fc9d6b0314
Nicojfc <nicolas_97jose@hotmail.com>
2023-08-20 19:51:32 -0300

    nuevos cambios

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -51,7 +51,7 @@ test("POST -> 'URL_BASE', should resturn status code 201 and res.body.title = pr
 
 })
 
-test("GET -> 'URL_BASE', should resturn status code 200 and res.body.legnth = 1", async () => {
+test("GET -> 'URL_BASE', should resturn status code 200 and res.body.length = 1", async () => {
 
   const res = await request(app)
     .get(URL_BASE)
@@ -78,7 +78,7 @@ test("GET ONE -> 'URL_BASE/:id', should resturn status code 200 and res.body.tit
 test("PUT -> 'URL_BASE/:id', should resturn status code 200 and res.body.title = productUpdate.title", async () => {
 
   const productUpdate = {
-    title: "Samsung oled 55",
+    title: "Anatomia PRO",
   }
 
   const res = await request(app)
@@ -91,11 +91,10 @@ test("PUT -> 'URL_BASE/:id', should resturn status code 200 and res.body.title =
   expect(res.body).toBeDefined()
   expect(res.body.title).toBe(productUpdate.title)
 
-  expect(res.body[0].category).toBeDefined()
-  expect(res.body[0].category.id).toBe(category.id)
+  
 })
 
-test("GET -> 'URL_BASE?category=id', should resturn status code 200 and res.body.legnth = 1, res.body[0].category to be defined and res.body[0].category = category.id", async () => {
+test("GET -> 'URL_BASE?category=id', should resturn status code 200 and res.body.length = 1, res.body[0].category to be defined and res.body[0].category = category.id", async () => {
   const res = await request(app) // /api/v1/products?category=1
     .get(`${URL_BASE}?category=${category.id}`)
   expect(res.status).toBe(200)
